7ce079552e4c96bf6e98263a542001d03bc4c7f4
Taekaboy <Taekaboy@Taekaboy>
2021-03-22 01:23:44 +0000

    Testing new commit

--- DS-Project-GerryGonzales-x17140493/src/main/java/GRPC/HeatingServer.java
+++ DS-Project-GerryGonzales-x17140493/src/main/java/GRPC/HeatingServer.java
@@ -66,7 +66,7 @@ public class HeatingServer extends HeatingServiceImplBase{
 			}
 
 		}
-	}
+	}//end method
 
 	public static void main(String[] args) throws IOException, InterruptedException {
 		startDiscovery();
