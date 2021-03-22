ad2127405a6034bbcf2f0a5513be0fe52714adf7
Taekaboy <Taekaboy@Taekaboy>
2021-03-22 00:07:47 +0000

    Added Registering to jmDNS package

---
+++ DS-Project-GerryGonzales-x17140493/src/main/java/jmDNS/Registering.java
@@ -0,0 +1,62 @@
+package jmDNS;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.HashMap;
+
+import javax.jmdns.JmDNS;
+import javax.jmdns.ServiceInfo;
+import Models.*;
+
+
+public class Registering {
+	public static JmDNS jmdns;
+	
+	// TODO Auto-generated method stub
+	public static void main(String[] args) throws UnknownHostException, IOException  {
+      	
+
+    }
+	
+	public void jmndsRegister(int heatingPort, int sprinklerPort, int lightPort, int camPort) throws InterruptedException {
+
+    	Heating myHeating = new Heating();
+    	Sprinkler mySprinkler = new Sprinkler();
+    	Light myLight = new Light();
+
+		try {
+			// Create a JmDNS instance
+			// JmDNS jmdns = JmDNS.create(InetAddress.getLocalHost());
+			jmdns = JmDNS.create(InetAddress.getLocalHost());
+			// Registering all services
+			System.out.println("Registering");
+			ServiceInfo serviceHeating = ServiceInfo.create("_http._tcp.local.", myHeating.getAppliance(), heatingPort,
+					"path=index.html");
+			ServiceInfo serviceProjector = ServiceInfo.create("_http._tcp.local.", mySprinkler.getAppliance(), sprinklerPort,
+					"path=index.html");
+			ServiceInfo serviceLight = ServiceInfo.create("_http._tcp.local.", myLight.getAppliance(), lightPort,
+					"path=index.html");
+
+			jmdns.registerService(serviceHeating);
+			jmdns.registerService(serviceProjector);
+			jmdns.registerService(serviceLight);
+
+			// Wait a bit
+			// Thread.sleep(25000);
+
+			// Unregister all services
+			// jmdns.unregisterAllServices();
+
+		} catch (IOException e) {
+			System.out.println(e.getMessage());
+		}
+	}
+
+	public void unRegister() {
+		System.out.println("Un-registering");
+
+		jmdns.unregisterAllServices();
+
+	}
+
+}
